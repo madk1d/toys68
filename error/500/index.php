48a0dcf8b805bd4728bad1893924ec8ec4ea2d5f
madk1d <55014015+madk1d@users.noreply.github.com>
2021-02-05 22:42:34 +0300

    Add files via upload

---
+++ error/500/index.php
@@ -0,0 +1,22 @@
+<?php
+header("HTTP/1.1 500 Internal Server Error");
+header("Status: 500 Internal Server Error");
+?>
+<!DOCTYPE HTML>
+<html lang="ru">
+<head>
+<title>500: Внутренняя ошибка сервера</title>
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8" >
+<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Numans" type="text/css" />
+<link rel="stylesheet" href="/assets/css/404.min.css" type="text/css" />
+<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+</head>
+<body>
+<div id="wrapper">
+<div style="text-align:center;">Внутренняя ошибка сервера. Попробуйте повторить позднее.</div>
+<div id="error">
+<img class="err" src="/images/404.jpg" alt="500: Internal Server Error" />
+</div> 
+</div>
+</body>
+</html>
