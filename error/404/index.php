659bba540eb19b5f4793ba2466397f52230ce5c4
madk1d <55014015+madk1d@users.noreply.github.com>
2021-02-05 22:42:04 +0300

    Add files via upload

---
+++ error/404/index.php
@@ -0,0 +1,23 @@
+<?php
+header("HTTP/1.1 404 Not Found");
+header("Status: 404 Not Found");
+?>
+<!DOCTYPE HTML>
+<html lang="ru">
+<head>
+<title>404: Не найдено</title>
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8" >
+<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Numans" type="text/css" />
+<link rel="stylesheet" href="/assets/css/404.min.css" type="text/css" />
+<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+</head>
+<body>
+<div id="wrapper">
+<div style="text-align:center;">Страница не найдена</div>
+<div style="text-align:center;margin-top:1em;"><a href="http://toys68.ru" class="c">Вернуться на главную</a></div>
+<div id="error">
+<img class="err" src="/images/404.jpg" alt="404: Not Found" />
+</div> 
+</div>
+</body>
+</html>
