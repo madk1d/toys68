9848f8b6d59243cb8a104d5599bb1f25806b4f8f
madk1d <55014015+madk1d@users.noreply.github.com>
2021-02-05 22:41:32 +0300

    Add files via upload

---
+++ error/403/index.php
@@ -0,0 +1,23 @@
+<?php
+header("HTTP/1.1 403 Forbidden");
+header("Status: 403 Forbidden");
+?>
+<!DOCTYPE HTML>
+<html lang="ru">
+<head>
+<title>403: Доступ закрыт</title>
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8" >
+<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Numans" type="text/css" />
+<link rel="stylesheet" href="/assets/css/404.min.css" type="text/css" />
+<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+</head>
+<body>
+<div id="wrapper">
+<div style="text-align:center;">Доступ закрыт!</div>
+<div style="text-align:center;margin-top:1em;"><a href="http://toys68.ru" class="c">Вернуться на главную</a></div>
+<div id="error">
+<img class="err" src="/images/404.jpg" alt="403: Forbidden" />
+</div> 
+</div>
+</body>
+</html>
